dec86b83038322ca7371afe5211fba2192d2145a
Leonardo Vaudagna <leovaudagna92@gmail.com>
2024-02-29 16:08:04 -0300

    add languaje

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@
 let resolution = 4;
 let brightness = 1;
 
+let lang = 'uk';
+
 //DOM Elements
 const container = document.getElementById('container');
 const resolutionButton = document.getElementById("change-resolution-button");
 const box = document.getElementById('box');
 const clearButton = document.getElementById('clear-button');
 const root = document.documentElement;
+const ukFlag = document.getElementById('uk');
+const spanishFlag = document.getElementById('es');
+const titlePage = document.querySelector('h1');
+const languajeDiv = document.getElementById('languaje');
+
 
 
 //Event listeners //
@@ -36,17 +43,10 @@ resolutionButton.addEventListener('click', function(){
     } 
 });
 
-
-
 //Change boxes color
 container.addEventListener('mouseover', function(e){
-    
-    // let paintedBox = true;
-
     let newRandomColor = randomColor();
-
     let targetElement = e.target;
-
     if(targetElement.id == 'box'){
         targetElement.style.backgroundColor = newRandomColor;
         targetElement.style.filter = `brightness(${brightness})`;
@@ -60,11 +60,10 @@ container.addEventListener('mouseover', function(e){
 
 //Clear grid button
 clearButton.addEventListener('click', function(){
-    cleanContainer();
-    renderContainer(1);
-
+    cleanContainer(resolution ** 2);
 });
 
+//Random color function
 function randomColor(){
     rgb = "";
     r = Math.floor(Math.random() * 256);
@@ -74,12 +73,13 @@ function randomColor(){
 }
 
 //PAGE RENDER
-function cleanContainer(){
+function cleanContainer(r){
     container.innerHTML = "";
+    renderContainer(r);
 }
 
 function renderContainer(r){
-    cleanContainer();
+    container.innerHTML = "";
     root.style.setProperty('--resoultion-number', resolution);
     for(x = 0; x < r; x++){
         let box = document.createElement('div');
@@ -88,9 +88,36 @@ function renderContainer(r){
     }
 }
 
+//Change languaje
+ukFlag.addEventListener('click', function(){
+    clearButton.innerText = "Clear";
+    resolutionButton.innerText = "Change Resolution";
+    titlePage.innerText = "Etch-A-Sketch";
+    spanishFlag.style.filter = 'brightness(0.5)';
+    ukFlag.style.filter = 'brightness(1)';
+    lang = 'uk';    
+})
+
+spanishFlag.addEventListener('click', function(){
+    clearButton.innerText = "Limpiar";
+    resolutionButton.innerText = "Cambiar Resolución";
+    titlePage.innerText = "Dibujo!"; 
+    spanishFlag.style.filter = 'brightness(1)';
+    ukFlag.style.filter = 'brightness(0.5)';
+    lang = 'es';    
+})
+
+ukFlag.addEventListener('mouseover', function(e){
+    e.target.style.filter = 'brightness(0.8)';
+})
+
+ukFlag.addEventListener('mouseout', function(e){
+    e.target.style.filter = 'brightness(0.5)';
+})
+
+
+
+
+
+
 renderContainer(resolution ** 2);
-
-//AGRGAR HOVER: mouseover
-//CADA VEZ QUE RENDERIZA LA PAGINA, borrar el grid
-
-
